09ff42bf7c0c7b59dd590356ecbf23b32d0478b1
Kayla Mendoza <mendozakayla00@gmail.com>
2025-12-31 13:57:58 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Welcome to my workout generator!")
+
+print("First, choose the type of workout you want:")
+print("1. Full Body")
+print("2. Upper Body")
+print("3. Lower Body")
+
+type = int(input("Enter the number of your choice: "))
+
+# create classes for different workout types
+
