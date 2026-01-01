720ac4870afded56b07ef99f4892982cbab60ad9
Kayla Mendoza <mendozakayla00@gmail.com>
2026-01-01 14:19:02 -0500

    feat: add UpperBody class

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
+import random
+
 print("Welcome to my workout generator!")
 
 print("First, choose the type of workout you want:")
-print("1. Full Body")
-print("2. Upper Body")
-print("3. Lower Body")
+print("1. Upper Body")
+print("2. Lower Body")
+print("3. Full Body")
+print("4. HIIT/SIT")
 
 type = int(input("Enter the number of your choice: "))
 
 # create classes for different workout types
+
+class UpperBody:
+
+    push_chest_compound = [
+        "Barbell Bench Press",
+        "Dumbbell Bench Press",
+        "Dumbell Incline Press",
+        "Push-Ups"]
+    
+    push_shoulder_compound = [
+        "Overhead Press",
+        "Dumbbell Shoulder Press",
+        "Upright Barbell Row",
+        ]
+    
+    pull
+    
+    upper_isolation = [
+        "Bicep Curls",
+        "Tricep Extension",
+        "Lateral Raise",
+        "Shoulder Press",
+    ]
+
+    def 
 
