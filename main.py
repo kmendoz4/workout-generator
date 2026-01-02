30a554c94ab89bcd36451f4d841362d62bdd7fde
Kayla Mendoza <mendozakayla00@gmail.com>
2026-01-02 11:26:34 -0500

    feat: working randomization for upper body workout

--- main.py
+++ main.py
@@ -10,35 +10,35 @@ print("4. HIIT/SIT")
 
 type = int(input("Enter the number of your choice: "))
 
-# create classes for different workout types
+upper_chest_compound = [
+    "Barbell Bench Press 3x6-8",
+    "Dumbbell Bench Press 3x6-8",
+    "Dumbell Incline Press 3x6-8",
+    "Deficit Push-Ups 3xfailure"]
+    
+upper_shoulder_compound = [
+    "Overhead Press 3x6-8",
+    "Dumbbell Shoulder Press 3x6-8",
+    "Upright Barbell Row 3x6-8",]
+    
+upper_back_compound = [
+    "Seated Row 3x6-8",
+    "Barbell Row 3x6-8",]
+    
+upper_lat_compound = [
+    "Lat Pulldowns 3x10-12",
+    "Pull-ups 3xfailure"]
+    
+upper_accessory = [
+    "Bicep Curls 2x10-12",
+    "Tricep Extension 2x10-12",
+    "Lateral Raise 2x10-12",]
 
-class UpperBody:
-
-    push_chest_compound = [
-        "Barbell Bench Press 3x6-8",
-        "Dumbbell Bench Press 3x6-8",
-        "Dumbell Incline Press 3x6-8",
-        "Deficit Push-Ups 3xfailure"]
-    
-    push_shoulder_compound = [
-        "Overhead Press 3x6-8",
-        "Dumbbell Shoulder Press 3x6-8",
-        "Upright Barbell Row 3x6-8",]
-    
-    pull_back_compound = [
-        "Seated Row 3x6-8",
-        "Barbell Row 3x6-8",]
-    
-    pull_lat_compound = [
-        "Lat Pulldowns 3x10-12",
-        "Pull-ups 3xfailure"
-    ]
-    
-    upper_accessory = [
-        "Bicep Curls 2x10-12",
-        "Tricep Extension 2x10-12",
-        "Lateral Raise 2x10-12",]
-    
-    def upper_body_generator():
-        
+if type == 1:
+    print(random.choice(upper_chest_compound))
+    print(random.choice(upper_shoulder_compound))
+    print(random.choice(upper_back_compound))
+    print(random.choice(upper_lat_compound))
+    print(random.choice(upper_accessory))
+    print(random.choice(upper_accessory))
 
