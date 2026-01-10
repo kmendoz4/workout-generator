09850c91d0b1bb96ffdf59cfb1e1ef6103b2de7c
Kayla Mendoza <mendozakayla00@gmail.com>
2026-01-10 11:54:30 -0500

    feat: add dictionary rather than lists

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import random
 
+class WorkoutTracker:
+    def __init__ (self):
+        self.workouts = { # workout dictionary
+            "squat" : {
+                "squat_variations" : ["Back Squat 3x6", "Front Squat 3x8-10", ""] #\n for enter
+            }
+        }
+
 print("Welcome to my workout generator!")
 
 print("\n1. Generate Squat Day Workout")
@@ -8,66 +16,3 @@ print("3. Generate Deadlift Day Workout")
 print("4. Gemerate HIIT/SIT Day Workout")
 
 type = int(input("Enter the number of your choice: "))
-
-upper_chest_compound = [
-    "Barbell Bench Press 3x6-8",
-    "Dumbbell Bench Press 3x6-8",
-    "Dumbell Incline Press 3x6-8",
-    "Deficit Push-Ups 3xfailure"]
-    
-upper_shoulder_compound = [
-    "Overhead Press 3x6-8",
-    "Dumbbell Shoulder Press 3x6-8",
-    "Upright Barbell Row 3x6-8",]
-    
-upper_back_compound = [
-    "Seated Row 3x6-8",
-    "Barbell Row 3x6-8",]
-    
-upper_lat_compound = [
-    "Lat Pulldowns 3x10-12",
-    "Pull-ups 3xfailure"]
-    
-upper_accessory = [
-    "Bicep Curls 2x10-12",
-    "Lateral Raise 2x10-12",]
-
-upper_isolation = [
-    "Dips 2x10-12",
-    "Tricep Extensions 2x10-12",]
-
-upper_workout = [
-    random.choice(upper_chest_compound),
-    random.choice(upper_shoulder_compound),
-    random.choice(upper_back_compound),
-    random.choice(upper_lat_compound),
-    random.choice(upper_accessory),
-    random.choice(upper_isolation)]
-
-lower_main_compound = [
-    "Squats 3x8-10",
-    "Deadlift 3x6-8"]
-
-lower_secondary_compound = [
-    "Lunges 3x8-10",
-    "Single Leg Leg Press 3x8-10",
-    "Hip Thrusts 3x8-10"]
-
-lower_accessory = [
-    "Leg Extension 3x8-10",
-    "Calf Raise 3x12"]
-
-lower_isolation = [
-    "Hamstring Curl 3x8-10",
-    "RDL 3x8-10"]
-
-lower_workout = [
-    random.choice(lower_main_compound),
-    random.choice(lower_secondary_compound),
-    random.choice(lower_accessory),
-    random.choice(lower_isolation)]
-
-if type == 1:
-    print(*upper_workout, sep='\n')
-elif type == 2:
-    print(*lower_workout, sep='\n')
