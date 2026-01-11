34c6d4012e03b8ea526b49919fc7e68374b8f541
Kayla Mendoza <mendozakayla00@gmail.com>
2026-01-11 11:36:47 -0500

    feat: add workouts dictionary

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import random
 
-class WorkoutTracker:
-    def __init__ (self):
-        self.workouts = { # workout dictionary
-            "squat" : {
-                "squat_variations" : ["Back Squat 3x6", "Front Squat 3x8-10", ""] #\n for enter
-            }
-        }
+workouts = {
+    "squat" : {
+        "squat_variations" : ["Back Squat 3x8", "Front Squat 3x8"],
+        "back" : ["Lat Pulldown 2x10-12 then Drop Set", "Pull-Ups 3x10-12", "Face Pulls 3x12\nBack Extensions 3x10",
+                  "Normal Grip Lat Pulldown 2x10-12\nWide Grip Lat Pulldown 2x10-12"],
+        "shoulder" : ["DB Shoulder Press 3x10\nRear Delt Flies 3x15", "BB Shoulder Press 3x10\nFront Raises3x12", "Cuban Press 4xAMRAP"],
+        "arms" : []
+    }
+}
 
 print("Welcome to my workout generator!")
 
